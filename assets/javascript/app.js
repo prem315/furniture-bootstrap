37e01728d3d229c5535ace2b60465d255f834082
Prem Patel <prem.patel3105@gmail.com>
2016-12-12 10:25:10 +0530

    created pages

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,3 +5,24 @@ $(document).ready(function(){
 	  }
 	});
 })
+
+$(document).ready(function(){
+	$(window).scroll(function() {
+	  if ($(document).scrollTop() > 50) {
+	    $('nav').addClass('shrink');
+	  } else {
+	    $('nav').removeClass('shrink');
+	  }
+	});
+});
+
+$(document).ready(function() {
+    $("div.bhoechie-tab-menu>div.list-group>a").click(function(e) {
+        e.preventDefault();
+        $(this).siblings('a.active').removeClass("active");
+        $(this).addClass("active");
+        var index = $(this).index();
+        $("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
+        $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
+    });
+});
